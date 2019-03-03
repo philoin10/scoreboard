6a021151aebcff013ef102799e87f816d20487be
Robin <philoin10@gmail.com>
2019-03-03 18:23:06 +0900

    AddPlayer 기능 추가.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Player} from "./components/Player";
+import {AddPlayerForm} from "./components/AddPlayerForm";
 
 class App extends React.Component {
   state = {
@@ -35,6 +36,24 @@ class App extends React.Component {
     }))
   }
 
+  // 플레이어 추가하는 메서드
+  handleAddPlayer = (playerName) => {
+    console.log("playerName", playerName)
+    let maxId = 0
+    this.state.players.forEach(item => {
+      maxId = item.id > maxId ? item.id : maxId
+    });
+
+    this.setState(prevState => {
+      return {
+        players: [
+          ...this.state.players,
+          {name: playerName, score: 0, id: maxId+1}
+        ]
+      }
+    })
+  }
+
   render() {
     return (
       <div className="scoreboard">
@@ -47,6 +66,8 @@ class App extends React.Component {
             changeScore={this.handleChangeScore} score={player.score} id={player.id}
           />)
         }
+
+        <AddPlayerForm addPlayer={(playerName) => this.handleAddPlayer(playerName)}/>
       </div>
     );
   }
